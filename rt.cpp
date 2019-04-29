7c3de0d9d600dce0a60fe5804b56287792e9df31
Johnathan Burns <jburns@oldi.com>
2019-04-29 17:34:56 -0400

    Added smoke

--- rt.cpp
+++ rt.cpp
@@ -3,6 +3,7 @@
 #include "moving_sphere.h"
 #include "rect.h"
 #include "box.h"
+#include "constant_medium.h"
 #include "solid_list.h"
 #include "float.h"
 #include "camera.h"
@@ -15,6 +16,8 @@ solid_list *two_spheres();
 solid_list *two_perlin_spheres();
 solid_list *simple_light();
 solid_list *cornell_box();
+solid_list *cornell_smoke();
+
 
 int main()
 {
@@ -42,7 +45,7 @@ int main()
 	solid_list *world = new solid_list(list, world_size);
 	*/
 
-	solid_list *world = cornell_box();
+	solid_list *world = cornell_smoke();
 
 	vec3 lookfrom(278, 278, -800);
 	vec3 lookat(278,278,0);
@@ -203,4 +206,29 @@ solid_list *cornell_box() {
 	list[i++] = new translate(new rotate_y(new box(vec3(0, 0, 0), vec3(165, 165, 165), white), -18), vec3(130,0,65));
 	list[i++] = new translate(new rotate_y(new box(vec3(0, 0, 0), vec3(165, 330, 165), white),  15), vec3(265,0,295));
 	return new solid_list(list,i);
+}
+
+solid_list *cornell_smoke()
+{
+	solid **list = new solid*[8];
+	int i = 0;
+	material *red = new lambertian( new constant_texture(vec3(0.65, 0.05, 0.05)) );
+	material *white = new lambertian( new constant_texture(vec3(0.73, 0.73, 0.73)) );
+	material *green = new lambertian( new constant_texture(vec3(0.12, 0.45, 0.15)) );
+	material *light = new diffuse_light( new constant_texture(vec3(15, 15, 15)) );
+
+	list[i++] = new flip_normals(new yz_rect(0, 555, 0, 555, 555, green));
+	list[i++] = new yz_rect(0, 555, 0, 555, 0, red);
+	list[i++] = new xz_rect(213, 343, 227, 332, 554, light);
+	list[i++] = new flip_normals(new xz_rect(0, 555, 0, 555, 555, white));
+	list[i++] = new xz_rect(0, 555, 0, 555, 0, white);
+	list[i++] = new flip_normals(new xy_rect(0, 555, 0, 555, 555, white));
+
+	solid *b1 = new translate(new rotate_y(new box(vec3(0, 0, 0), vec3(165, 165, 165), white), -18), vec3(130, 0, 65));
+	solid *b2 = new translate(new rotate_y(new box(vec3(0, 0, 0), vec3(165, 330, 165), white), 15), vec3(265, 0, 295));
+
+	list[i++] = new constant_medium(b1, 0.01, new constant_texture(vec3(1.0, 1.0, 1.0)));
+	list[i++] = new constant_medium(b2, 0.01, new constant_texture(vec3(0.0, 0.0, 0.0)));
+
+	return new solid_list(list, i);
 }
